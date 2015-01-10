d37943c5bff94abe55ac7be609a943c919bb5859
practicalparticipation <tim@practicalparticipation.co.uk>
2015-01-10 16:50:54 +0000

    Updated schema

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
+![360 Giving Logo](http://openphilanthropy.files.wordpress.com/2014/02/cropped-360givinglogo-2010-size.jpg)
+
 360 Giving Standard
 ========
 
-See [http://threesixtygiving.com/](http://threesixtygiving.com/) for more details. 
+360 Giving aims to help UK grant makers and philanthropists to publish their grant information online in an easy to use way.  We are motivated by a commitment to the value of open data and strategic funding.
 
-This repository right now used for tracking issues. 
+The **360 Giving Data Standard** provides a common approach for publishing grant data. 
 
-More to come. 
+For more about the overall project, visit the main [360 Giving website](http://threesixtygiving.com/about/).
+
+Full documentation of the data standard is provided at [http://threesixtygiving.github.io](http://threesixtygiving.github.io)
+
+## Schema and tools
+
+From Version 1.0 of the schema and onwards:
+
+* The authoritative source of the standard is found in 360-giving-schema.json in [JSON Schema](http://json-schema.org/) format.
+* Two secondary serialisation templates are generated using the [flatten-ocds toolkit](https://github.com/open-contracting/flattening-ocds): a summary table (CSV/XLS), and multi-table template (XLS)
+* A JSON Table Schema is provided for the summary table
+
+This repository stores the latest versions of the schema, along with tools used in generating secondary serialisations and documentation.
+
---
+++ tools/summary-table-schema.py
@@ -0,0 +1,65 @@
+import json
+from collections import OrderedDict
+import jsonref
+import sys 
+
+table_schema = []
+
+with open("../360-giving-schema.json") as schema_file:
+    data = jsonref.load(schema_file,object_pairs_hook=OrderedDict)
+
+def get_field_info(field_def):
+    validation = ''
+    if 'string' in field_def['type']:
+        suffix = ''
+        field_format = ''
+        if 'date-time' in field_def.get('format',''):
+            field_type = 'datetime'
+        else:
+            field_type = 'string'
+
+        if field_def.get('enum',False):
+            validation = ",".join(field_def.get('enum'))
+        
+    elif 'number' in field_def['type']:
+        suffix = ':number'
+        field_type = 'number'
+        field_format = ''
+    elif 'integer' in field_def['type']:
+        suffix = ':integer'
+        field_type = 'integer'
+        field_format = ''
+    elif 'boolean' in field_def['type']:
+        suffix = ':boolean'
+        field_type = 'boolean'
+        field_format = ''
+    
+    return {"suffix":suffix,"type":field_type,"format":field_format,"validation":validation}
+    
+    
+for name, defs in data['properties'].iteritems():
+    field = {}
+    
+    if 'array' in defs['type'] or 'object' in defs['type']:
+        try: # If we have rollUp properties, then include these in the summary table 
+            for item in defs['rollUp']:
+                subfield = {}
+                subdefs = defs['items']['properties'][item]
+                field_info = get_field_info(subdefs)
+                subfield.update({"name":name + "/"+ item + field_info['suffix'],'title':defs['title'] + ":" + subdefs['title'],'description':subdefs.get('description','-'),"type":field_info["type"],"format":field_info["format"]})
+                table_schema.append(subfield)
+        except Exception as a:
+            if defs['items'] == 'string':
+                field.update({"name":name,'title':defs['title'],'description':defs.get('description','-'),'type':'array','format':'string'})
+    else:
+        field_info = get_field_info(defs)
+        field.update({'name':name,'title':defs['title'],'description':defs.get('description','-'),"type":field_info["type"],"format":field_info["format"]})
+        if field_info['validation']:
+            field.update({"allowed_values":field_info['validation']})
+    
+        table_schema.append(field)
+
+print "Writing updated schema"
+
+with open('../360-summary-json-table-schema.json', 'w') as outfile:
+    json.dump({"fields":table_schema}, outfile,indent=True)
