aa1a5e9f978e5192216738f558f152dad20412de
practicalparticipation <tim@practicalparticipation.co.uk>
2015-01-31 18:09:58 +0000

    Updating geography for point locations (closes #38) and ward code gazeteers (#29)

---
+++ tools/fetch-geographic-areas.py
@@ -0,0 +1,31 @@
+## Convenience script for building the codelist of geographic code areas.
+import json
+import urllib2
+import csv
+
+def fetch_items(url):
+    response = urllib2.urlopen(url)
+    json_data = json.loads(response.read())
+    
+    rows = []
+    for item in json_data['result']['items']:
+        try:
+            if item['status'] == 'live' and int(item.get('liveinstances',0)) > 8:
+                rows.append([item['abbreviation'],item['name'],"An ONS "+item['theme'] + " codelist",item['_about'],item['firstcode']])
+        except:
+            pass
+        
+    if json_data['result'].get('next',''):  
+        print json_data['result'].get('next','')
+        rows = rows + fetch_items(json_data['result'].get('next',''))
+    return rows
+
+rows = fetch_items('http://statistics.data.gov.uk/doc/statistical-entity/.json?_pageSize=50&_view=full&_page=1')
+with open('../codelists/geoCodeType-temp.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['Code','Name','Description','Documentation (URL)','Values (URL)','ONS Code'])   
+    for row in rows:
+        writer.writerow(row)
+
+    
+            
