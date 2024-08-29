06c973588846e38b1b4890d2ac2ff1eb1aa3dd67
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-08-29 15:50:42 +0100

    documentation/conf.py: New RTD config requirements

--- documentation/conf.py
+++ documentation/conf.py
@@ -27,6 +27,17 @@ sys.path.insert(0, os.path.abspath('.'))
 from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
 
+# -- Read the Docs --------------------------------------------------------
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
